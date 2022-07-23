8616869ea34dbc84a1f2c0f226e6472ba3e6003d
Tolga Köyluoglu <tolga.koyluoglu1@gmail.com>
2022-07-23 19:26:10 +0200

    test

--- Dockerfile
+++ Dockerfile
@@ -10,3 +10,5 @@ COPY . .
 
 EXPOSE 4000
 CMD ["npm", "start"]
+
+ENV NODE_OPTIONS=--max_old_space_size=2048
--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,11 @@
 import express from 'express'
 const app = express()
 // import './config/redis'
-
-const { NODE_ENV } = process.env
-
+console.log('hello')
 // env variables for client
-const allowedOrigins: string[] = ['http://0.0.0.0:3000', 'http://localhost:3000']
-if (NODE_ENV === 'development' || NODE_ENV === 'staging') {
-  allowedOrigins.push('')
-}
-
-app.use((req, res, next) => {
-  const { origin }: any = req.headers
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin)
-  }
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type')
-  res.setHeader('Access-Control-Allow-Credentials', true.toString())
-
-  next()
-})
-
-import bodyParser from 'body-parser'
-app.use(bodyParser.json({ limit: '50mb' }))
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }))
-
-import cookieParser from 'cookie-parser'
-app.use(cookieParser())
 
 import { logRequests } from './helpers/logger'
 app.use(logRequests())
-
-import router from './routes'
-app.use(router)
-
-import swaggerUi from 'swagger-ui-express'
-if (NODE_ENV === 'development') {
-  const swaggerDocument = require('./config/swagger/index')
-  router.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
-}
 
 const PORT: number = parseInt(process.env.PORT as string) || 3000
 const HOST: string = process.env.HOST || '0.0.0.0'
